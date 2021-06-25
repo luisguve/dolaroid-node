4919ccec1ec94d82024323078198327616278dd0
Luis G. Villegas <luisguve@gmail.com>
2021-06-25 17:36:17 -0300

    Updated list of allowed origins for CORS

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(cors({
-  origin: ["http://localhost:5000", "https://dolaroid.netlify.app"],
+  origin: ["http://localhost:3000", "https://dolaroid.netlify.app"],
   credentials: true
 }))
 app.use(cookieSession({
